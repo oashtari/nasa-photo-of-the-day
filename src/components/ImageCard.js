505fd6b3f4e82799bc98c9cabbb35b54c76d24c2
Omid Ashtari <omid@Omids-Mac-mini-2.local>
2019-11-19 15:59:20 -0800

    completed step 2 with basic file structure for image card and image list

---
+++ src/components/ImageCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImageCard = props => {
+    return (
+        <div className="images">
+            <h3>Title</h3>
+            <p>explanation</p>
+            <p>date</p>
+        </div>
+    )
+}
+
+export default ImageCard;
